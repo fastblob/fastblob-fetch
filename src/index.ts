673f3260f4212a2c86c208adf1fea63f2a289727
Seedgou <i@zczc.cz>
2022-11-19 08:59:11 +0000

    add fetch function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
+import fastBlobFetch from '@fastblob/fast-fetch'
+
+import { getFastBlobInputs } from './urls'
+
+type FetchParams = Parameters<typeof fastBlobFetch>
+type FetchReturn = ReturnType<typeof fastBlobFetch>
+
+async function fetch (input: FetchParams[0], init: FetchParams[1]): FetchReturn {
+  const fastblobURLs = getFastBlobInputs(input)
+  const newInput = fastblobURLs[0]
+  const mirrors = fastblobURLs.slice(1)
+
+  const newInit = init ?? {}
+
+  // @ts-expect-error
+  newInit.fastFetch = newInit?.fastFetch ?? {}
+  // @ts-expect-error
+  newInit.fastFetch.mirrorURLs = [...mirrors, ...newInit.fastFetch.mirrorURLs ?? []]
+
+  return await fastBlobFetch(newInput, newInit)
+}
+
 export default fetch
